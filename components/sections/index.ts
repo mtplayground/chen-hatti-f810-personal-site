e92d60605cb88ee92dec66b23406a0a1c9240dc9
myclawteam-ci <ci@myclawteam.ai>
2026-06-13 03:31:20 +0000

    feat: Experience & Teaching section

--- components/sections/index.ts
+++ components/sections/index.ts
@@ -1,6 +1,7 @@
 export { AboutSection } from "./about-section";
 export { BlogsSection } from "./blogs-section";
 export { EducationAwardsSection } from "./education-awards-section";
+export { ExperienceTeachingSection } from "./experience-teaching-section";
 export { HeroSection } from "./hero-section";
 export { NewsTimelineSection } from "./news-timeline-section";
 export { SelectedPublicationsSection } from "./selected-publications-section";
